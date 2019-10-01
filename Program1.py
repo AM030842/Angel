d2f87397d17bfb11a883b29f29907bd6c9d6d220
AM030842 <55776405+AM030842@users.noreply.github.com>
2019-10-01 10:58:15 -0500

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,57 @@
+print("This program will output the average, max, and min of any given positive numbers.")
+print("You can stop the input of numbers by inputting any negative number.")
+run = True
+
+#asks user if they want to try again
+def reRun():
+    run = True
+    response = input("would you like to try again? (yes or no): ")
+    if response.lower() == "no":
+        print("Thank you for using this program")
+        run = False
+        return run
+    print("----------------------------------------------")
+    return run
+
+#gets all the numbers the user wants to calculates
+def getNumbers():
+    amount = 0
+    tSum = 0
+    max = 0
+    min = 10000000000000000
+    while True:
+        numString = input("Please enter a number: ")
+        checkNegativeNum = numString.lstrip("-")
+        if not checkNegativeNum.isdigit():
+            print("Please make you sure you enter a valid number")
+            continue
+        else:
+            num1 = float(numString)
+        if num1 < 0:
+            break
+        else:
+            tSum = num1 + tSum
+            amount = amount + 1
+            if num1 > max:
+                max = num1
+            if num1 < min:
+                min = num1
+    return tSum,amount,max,min
+
+#calculates the average from the inputs
+def calculateAvg(tSum,amount):
+    average = tSum / amount
+    return average
+
+#prints the values
+def printValues(average, max, min):
+    print("The average of these values are", average)
+    print("The maximum of your values is", max)
+    print("The minimum of your values is", min)
+
+#calls on methods to run program
+while run:
+    tSum, amount, max, min = getNumbers()
+    average = calculateAvg(tSum,amount)
+    printValues(average, max, min)
+    run = reRun()
